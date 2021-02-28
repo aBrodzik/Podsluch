07222e7a5acc1b9827aac85375a0389d07bf6acc
Adam Brodzik <a.brodzik.1@stud.elka.pw.edu.pl>
2021-02-28 10:29:06 +0100

    helloWorld 1.1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from array import array
+
 from flask import Flask, redirect, url_for, render_template, request
 from os import listdir
 from os.path import isfile, join
@@ -12,21 +14,63 @@ def home():
 
 @app.route("/records", methods=['GET', 'POST'])
 def records():
-
-    #pobierz liste plików z katalogu, przyda się z tego zrobić oddzielną funkcję
-    path= "static/records"
+    # pobierz liste plików z katalogu, przyda się z tego zrobić oddzielną funkcję
+    path = "static/records"
     onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 
-    records=request.form.getlist('records[]')
+    records = request.form.getlist('records[]')
     if not records:
         records = onlyfiles;
     print('records to display', records)
     return render_template("records.html", records=records)
 
+
 @app.route("/settings")
 def settings():
     return render_template("settings.html")
 
+
+@app.route("/connect", methods=["GET", "POST"])
+def connect():
+    if request.method == "POST":
+        print("connecting...")
+
+    return render_template("index.html")
+
+
+@app.route("/onoff", methods=["GET", "POST"])
+def onoff():
+    if request.method == 'POST':
+        if request.form["off"] == "off":
+            print("turning off")
+        elif request.form["on"] == "on":
+            print("turning on")
+
+    return render_template("index.html")
+
+
+@app.route("/rec", methods=["GET", "POST"])
+def rec():
+    if request.method == "POST":
+        print("recording")
+
+    return render_template("index.html")
+
+
+@app.route("/sb", methods=["GET", "POST"])
+def standby():
+    if request.method == "POST":
+        print("staring standby")
+
+    return render_template("index.html")
+
+
+@app.route("/live", methods=["GET", "POST"])
+def live():
+    if request.method == "POST":
+        print("live")
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
