9b5b915383c3fd4ba941aa0fc0c3573bd5d1ae19
Adam Brodzik <a.brodzik.1@stud.elka.pw.edu.pl>
2021-03-02 00:30:33 +0100

    flask i ślepy zaułek

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from array import array
 
-from flask import Flask, redirect, url_for, render_template, request
+import audio
+from flask import Flask,   render_template, request
 from os import listdir
 from os.path import isfile, join
 
 app = Flask(__name__)
 
 
-@app.route("/",  methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         if request.form.get("off"):
@@ -15,18 +15,17 @@ def home():
         elif request.form.get("on"):
             print("turning on")
         elif request.form.get("connect"):
-                print("connecting...")
+            print("connecting...")
         elif request.form.get("start"):
-                print("starting recoring")
+            print("starting recoring")
         elif request.form.get("stop"):
-                print("stopping recording")
+            print("stopping recording")
         elif request.form.get("standby"):
-                print("turning on standby")
+            print("turning on standby")
         elif request.form.get("standaway"):
-                print("shutting down standby")
+            print("shutting down standby")
         elif request.form.get("live"):
-                print("starting live")
-
+            print("starting live")
 
     return render_template("index.html")
 
@@ -41,13 +40,27 @@ def records():
     if not records:
         records = onlyfiles;
     print('records to display', records)
+
+
+    if request.method == 'POST':
+        i=0;
+        for record in records:
+            i=i+1
+            print(request.form.get(str(i)))
+            if request.form.get(str(i)):
+                print("playing");
+                audio.play("static/records/"+records[i-1])
+                break
+
     return render_template("records.html", records=records)
+
+
+
 
 
 @app.route("/settings")
 def settings():
     return render_template("settings.html")
-
 
 
 if __name__ == "__main__":
