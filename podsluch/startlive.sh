808aafb22d9246706473fb9b930c1696fa8ecaa8
Piotr Marcin Mysłek <p.myslek@stud.elka.pw.edu.pl>
2021-04-10 17:22:02 +0000

    Update startlive.sh

--- podsluch/startlive.sh
+++ podsluch/startlive.sh
@@ -1,1 +1,2 @@
-rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null | sox -t raw -r 12.5k -e s -b 16 -c 1 -V1 - -t wav /home/podsluch/siodemka-master/podsluch/wiretapp/static/live/trueLiveTest.wav sinc 300-3400 lowpass 1887> /dev/null &
+#rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null | sox -t raw -r 12.5k -e s -b 16 -c 1 -V1 - -t wav /home/podsluch/siodemka-master/podsluch/wiretapp/static/live/trueLiveTest.wav sinc 300-3400 lowpass 1887> /dev/null &
+rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null|play -r 12.5k -t raw -e s -b 16 -c 1 -V0 - 2> /dev/null&
