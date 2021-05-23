d4228f6209b628726ab7d40ff6d9203193645b5a
Piotr Marcin Mysłek <p.myslek@stud.elka.pw.edu.pl>
2021-05-23 13:47:11 +0000

    zmiana odtwarzania na transmisje

--- podsluch/startlive.sh
+++ podsluch/startlive.sh
@@ -1,2 +1,7 @@
 #rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null | sox -t raw -r 12.5k -e s -b 16 -c 1 -V1 - -t wav /home/podsluch/siodemka-master/podsluch/wiretapp/static/live/trueLiveTest.wav sinc 300-3400 lowpass 1887> /dev/null &
-rtl_fm -f 446.14375M -E dc -g 0 -s 12.5k 2> /dev/null|play -r 12.5k -t raw -e s -b 16 -c 1 -V0 - sinc 300-3400 lowpass 1887 2> /dev/null&
+#rtl_fm -f 446.14375M -E dc -g 0 -s 12.5k 2> /dev/null|play -r 12.5k -t raw -e s -b 16 -c 1 -V0 - sinc 300-3400 lowpass 1887 2> /dev/null&
+
+#to w drugiej linijce bylo do kamienia milowego
+
+#czasem trzeba wylaczyc firewall na serwerze zeby dzialalo, np "sudo ufw disable"
+rtl_fm -f 446.14375M -E dc -g 0 -s 12.5k 2> /dev/null|sox -r 12.5k -t raw -e s -b 16 -c 1 -V0 - -t wav - sinc 300-3400 lowpass 1887 2> /dev/null | nc -l 8001&
