61eb4e896cb4cc5028656493a4fbe0bc83ac7994
Piotr Marcin Mysłek <p.myslek@stud.elka.pw.edu.pl>
2021-06-12 18:54:34 +0000

    dodanie daty

--- podsluch/start.sh
+++ podsluch/start.sh
@@ -1,1 +1,8 @@
-rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null | sox -t raw -r 12.5k -e s -b 16 -c 1 -V1 - -t wav /home/podsluch/siodemka-master/podsluch/wiretapp/static/records/nagranie.wav sinc 300-3400 lowpass 1887> /dev/null &
+data1=$(date --rfc-3339=d)
+data2=$(date --rfc-3339=s)
+data3=$(echo $data2 | awk '{ print substr( $0, 12 ) }')
+dash=-
+sciezka=/home/podsluch/siodemka-master4/podsluch/wiretapp/static/records/nagranie
+rozszerzenie=.wav
+polaczone=$sciezka$data1$dash$data3$rozszerzenie
+rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null | sox -t raw -r 12.5k -e s -b 16 -c 1 -V1 - -t wav $polaczone sinc 300-3400 lowpass 1887 > /dev/null &
