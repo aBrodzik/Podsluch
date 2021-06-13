6d68d904fd907592ec240fecbd1e5fccf66658f1
Piotr Marcin Mysłek <p.myslek@stud.elka.pw.edu.pl>
2021-06-13 23:29:43 +0000

    usuniecie strefy czasowej z nazwy pliku nagrania

--- podsluch/start.sh
+++ podsluch/start.sh
@@ -1,8 +1,9 @@
 data1=$(date --rfc-3339=d)
 data2=$(date --rfc-3339=s)
 data3=$(echo $data2 | awk '{ print substr( $0, 12 ) }')
+data4=$(echo $data3 | awk '{ print substr( $0, 1, length($0)-6 ) }')
 dash=-
-sciezka=/home/podsluch/siodemka-master4/podsluch/wiretapp/static/records/nagranie
+sciezka=/home/podsluch/siodemka-master/podsluch/wiretapp/static/records/nagranie
 rozszerzenie=.wav
-polaczone=$sciezka$data1$dash$data3$rozszerzenie
+polaczone=$sciezka$data1$dash$data4$rozszerzenie
 rtl_fm -f 446.14375M -E dc -s 12.5k 2> /dev/null | sox -t raw -r 12.5k -e s -b 16 -c 1 -V1 - -t wav $polaczone sinc 300-3400 lowpass 1887 > /dev/null &
