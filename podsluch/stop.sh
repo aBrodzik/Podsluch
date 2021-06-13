2c4bfbf4f05b7a9fa0e39ef83c65c4fd7fd0b5ba
Piotr Marcin Mysłek <p.myslek@stud.elka.pw.edu.pl>
2021-06-13 23:31:51 +0000

    Update stop.sh

--- podsluch/stop.sh
+++ podsluch/stop.sh
@@ -1,2 +1,4 @@
 rtl_fm_pid=$(pidof rtl_fm) && kill $rtl_fm_pid
 nc_pid=$(pidof nc) && kill $nc_pid
+ffmpeg_pid=$(pidof ffmpeg) && kill $ffmpeg_pid
+icecast2_pid=$(pidof icecast2) && kill $icecast2_pid
